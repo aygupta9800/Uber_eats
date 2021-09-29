7fe4947341ef1d436ab5f97e836e5af09c8ca55c
aygupta9800 <ayushgupta9800@gmail.com>
2021-09-29 12:11:25 -0700

    Adding redux persist to preserve redux state on page refresh

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,14 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit';
-import mainReducer from './reducers/mainSlice'
+import mainReducer from './reducers/mainSlice';
+import { combineReducers } from 'redux';
 import restaurantReducer from './reducers/restaurantSlice';
 import customerReducer from './reducers/customerSlice';
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const reducers = combineReducers({
+    mainReducer: mainReducer,
+    restaurantReducer,
+    customerReducer,
+  });
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+  
 // 
 
 export default configureStore({
-    reducer: {
-        mainReducer,
-        restaurantReducer,
-        customerReducer,
-    }
+    reducer: persistedReducer,
 
 })
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,13 +6,19 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import store from './app/store'
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <App />
+      </Router>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
