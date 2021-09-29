da59a0dd75cde28f2bdeddca4668c13e0b85eb8e
aygupta9800 <ayushgupta9800@gmail.com>
2021-09-29 12:09:32 -0700

    Adding api changes for res orders and search on res list

--- backend/routes/restaurants.js
+++ backend/routes/restaurants.js
@@ -78,23 +78,36 @@ router.put('/profile', auth, async (req, res) => {
 // TODO: add auth
 router.get(`/`, async (req, res) => {
     // console.log("req.query", req.query)
-    const { customer_city } =  req.query;
+    const { customer_city, search } =  req.query;
     const queryPromise1 = () => {
         // const sql1 = `SELECT * from restaurants;`;
-        const sql1 = `select r.res_id, r.email, r.address_id, r.name, r.phone_number, r.delivery_option, r.description, r.timing_open, r.timing_close,
-        a.street_address, a.apt_number, a.city, a.state, a.country, a.zipcode
-        from restaurants as r INNER JOIN addresses as a ON r.address_id = a.address_id order by a.city= '${customer_city}' DESC;`;
+        let sql1;
+        let search_string = `%${search}%`
+        if (!search) {
+         sql1 = `select r.res_id, r.email, r.address_id, r.name, r.phone_number, r.delivery_option, r.description, r.timing_open, r.timing_close,
+            a.street_address, a.apt_number, a.city, a.state, a.country, a.zipcode
+            from restaurants as r INNER JOIN addresses as a ON r.address_id = a.address_id order by a.city= '${customer_city}' DESC;`;
+            
+        }
+        else {
+            sql1 = `select DISTINCT r.res_id, r.email, r.address_id, r.name, r.phone_number, r.delivery_option, r.description, r.timing_open, r.timing_close,
+            a.street_address, a.apt_number, a.city, a.state, a.country, a.zipcode
+            from restaurants as r INNER JOIN addresses as a ON r.address_id = a.address_id Inner join restaurant_menu as d ON r.res_id = d.res_id where
+            d.dish_name LIKE '${search_string}' or r.name LIKE '${search_string}' or d.description LIKE '${search_string}' or a.city LIKE '${search_string}'
+            order by a.city= '${customer_city}' DESC;`;
+        }
         console.log("sql1:", sql1);
-        return new Promise((resolve, reject)=>{
-            pool.query(sql1,  (error1, result1)=>{
-                if(error1){
-                    console.log("error1:", error1);
-                    return reject(error1);
-                }
-                console.log("result1:", result1);
-                return resolve(result1);
+            return new Promise((resolve, reject)=>{
+                pool.query(sql1,  (error1, result1)=>{
+                    if(error1){
+                        console.log("error1:", error1);
+                        return reject(error1);
+                    }
+                    console.log("result1:", result1);
+                    return resolve(result1);
+                });
             });
-        });
+       
     };
     try {
         const result1 = await queryPromise1();
@@ -184,7 +197,7 @@ router.post('/:id/dish', auth, async (req, res) => {
 
 // update a restaurant dish
 
-router.put('/:res_id/dish/:id', auth, async (req, res) => {
+router.put('/:res_id/dish/:id', async (req, res) => {
     const { dish_name, dish_image, dish_price, description, main_ingredient, dish_category, food_type} = req.body;
     const res_id = req.params.res_id;
     const res_menu_id = req.params.id;
@@ -276,15 +289,79 @@ router.delete('/:res_id/dish/:id', auth, async (req, res) => {
     }
 });
 
-export default router;
+router.get('/:id/orders', async (req, res) => {
+    const res_id = req.params.id;
+    const queryPromise1 = () => {
+        const sql1 = `select o.order_id, o.customer_id, o.order_date_time, o.delivery_date_time, o.delivery_address,
+        o.delivery_status,o.taxes, o.tip, o.instruction, o.total_amount, o.delivery_type, c.first_name , c.last_name, c.email, c.phone_number, c.profile_pic
+        from orders as o Inner Join customers as c on o.customer_id = c.customer_id where o.res_id='${res_id}' order by order_date_time DESC;`;
+        console.log("sql1:", sql1);
+        return new Promise((resolve, reject)=>{
+            pool.query(sql1,  (error1, result1)=>{
+                if(error1){
+                    console.log("error1:", error1);
+                    return reject(error1);
+                }
+                console.log("result1:", result1);
+                return resolve(result1);
+            });
+        });
+    };
+    try {
+        const result1 = await queryPromise1();
+        // console.log("result1[0]:", result1);
+        let res_body = { data: result1};
+        return res.status(200).json(res_body);
+    } catch(error) {
+        console.log(error);
+        return res.status(500).json(error);
+    }
+});
 
-// let req = 
-// {
-//     "dish_name": "cake",
-//     "dish_image": "",
-//     "dish_price": 9.5,
-//     "description": "",
-//     "main_ingredient": "sdfdf",
-//     "dish_category": 'desserts',
-//     "food_type": 1
-// }
+router.put('/order', async (req, res) => {
+    const { order_id, delivery_status, res_id} = req.body;
+    const queryPromise1 = () => {
+        const sql1 = `update orders set delivery_status='${delivery_status}' where order_id = '${order_id}';`
+        console.log("sql1:", sql1);
+        return new Promise((resolve, reject)=>{
+            pool.query(sql1,  (error1, result1)=>{
+                if(error1){
+                    console.log("error1:", error1);
+                    return reject(error1);
+                }
+                console.log("result1:", result1);
+                return resolve(result1);
+            });
+        });
+    }
+    const queryPromise2 = () => {
+        const sql2 = `select o.order_id, o.customer_id, o.order_date_time, o.delivery_date_time, o.delivery_address,
+        o.delivery_status,o.taxes, o.tip, o.instruction, o.total_amount, o.delivery_type, c.first_name , c.last_name, c.email, c.phone_number, c.profile_pic
+        from orders as o Inner Join customers as c on o.customer_id = c.customer_id where o.res_id='${res_id}' order by order_date_time DESC;`;
+        // const sql2 = `SELECT * from orders where customer_id='${customer_id}';`;
+        console.log("sql2:", sql2);
+        return new Promise((resolve, reject)=>{
+            pool.query(sql2,  (error2, result2)=>{
+                if(error2){
+                    console.log("error2:", error2);
+                    return reject(error2);
+                }
+                // console.log("result2:", result2); 
+                return resolve(result2);
+            });
+        });
+    };
+    try {
+        const result1 = await queryPromise1();
+        const result2 = await queryPromise2();
+        // console.log("result1[0]:", result1);
+        let res_body = { data: result2};
+        return res.status(200).json(res_body);
+    } catch(error) {
+        console.log(error);
+        return res.status(500).json(error);
+    }
+});
+
+
+export default router;
