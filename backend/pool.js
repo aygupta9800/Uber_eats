a0c2847cb2f7d2f0ba21a3d31093d2b1d7182699
aygupta9800 <ayushgupta9800@gmail.com>
2021-10-04 20:00:33 -0700

    use of mysql with and without connection pooling

--- backend/pool.js
+++ backend/pool.js
@@ -1,6 +1,29 @@
-import mysql from "mysql";
+import mysql, { createConnection } from "mysql";
+
+// In createConnection method, We are creating a MySQL connection on every request coming from the user.
+//  Soon after getting multiple concurrent requests, the MySQL server will get overloaded and throw an error.
+
+// Connection Pooling is a mechanism to maintain a cache of database connection 
+// so that the connection can be reused after releasing it.
+// connection pooling helps to improve the performance of MySQL
+// and not overload the MySQL server with too many connections.
 
 const port = 3306;
+// const connection = mysql.createConnection({
+//     host: "database-1.caorc3vfgkkg.us-east-2.rds.amazonaws.com",
+//     user: "admin",
+//     password: "Password123",
+//     database: "ubereats_db",
+//     port,
+//     // connectionLimit: 100,
+// });
+
+// connection.connect((err) => {
+//     if (err) throw err;
+//     console.log('Connected to MySQL Server!');
+//   });
+
+// export default connection;
 const pool = mysql.createPool({
     host: "database-1.caorc3vfgkkg.us-east-2.rds.amazonaws.com",
     user: "admin",
