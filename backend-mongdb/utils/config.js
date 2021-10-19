7ccf2976cd5e1231cb7a687091be86e5bcc39b96
aygupta9800 <ayushgupta9800@gmail.com>
2021-10-19 12:13:49 -0700

    Initail Mongodb setup

---
+++ backend-mongdb/utils/config.js
@@ -0,0 +1,6 @@
+const config = {
+    mongoDB: 'mongodb+srv://root:root12345@cluster0.gyxjb.mongodb.net/UberEats?retryWrites=true&w=majority',
+    "token_key": "jwt_ubereats",
+};
+
+export default config;
--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,5 @@
 const config = {
     "token_key": "jwt_ubereats",
-    "frontend": "http://localhost:8000"
 }
 
 export default config;
