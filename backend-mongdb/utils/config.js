2aa80208e913f1ab97b591c5bd8211ed4e940a82
aygupta9800 <ayushgupta9800@gmail.com>
2021-10-19 12:53:48 -0700

    deleting old folder

--- backend-mongdb/utils/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    mongoDB: 'mongodb+srv://root:root12345@cluster0.gyxjb.mongodb.net/UberEats?retryWrites=true&w=majority',
-    "token_key": "jwt_ubereats",
-};
-
-export default config;
