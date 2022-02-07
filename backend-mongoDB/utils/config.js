14d4475c5c8a2de5e5d1fe4f5ff205153b2452df
aygupta9800 <ayushgupta9800@gmail.com>
2022-02-07 11:16:54 -0800

    removing cred

--- backend-mongoDB/utils/config.js
+++ backend-mongoDB/utils/config.js
@@ -1,12 +1,10 @@
 const config = {
-    mongoDB: 'mongodb+srv://root:root12345@cluster0.gyxjb.mongodb.net/UberEats?retryWrites=true&w=majority',
+    mongoDB: 'mongodb+srv://xxxx:xxxxxx@cluster0.gyxjb.mongodb.net/xxxxx?retryWrites=true&w=majority',
     token_key: 'jwt_ubereats',
-    // awsAccessKey: "AKIAQYDJN6YJATVACL6O",
-    // awsSecretKey: "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",
     bucketName: "ubereatsayush",
     bucketRegion: "us-east-2",
-    awsAccessKey: "AKIA5WQUGNR72TFV7UUD",
-    awsSecretKey: "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",
+    awsAccessKey: "xxxxxxxx",
+    awsSecretKey: "xxxxxxxx",
   }
 
 export default config;
