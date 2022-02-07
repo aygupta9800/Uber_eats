9ee7982fd4cf1e124ff933548c3ea395cef1697e
aygupta9800 <ayushgupta9800@gmail.com>
2022-02-07 10:15:24 -0800

    removing config cred

--- backend-mongoDB/utils/config.js
+++ backend-mongoDB/utils/config.js
@@ -1,12 +1,11 @@
+// set ur config here or in .env file
 const config = {
-    mongoDB: 'mongodb+srv://root:root12345@cluster0.gyxjb.mongodb.net/UberEats?retryWrites=true&w=majority',
+    mongoDB: 'mongodb+srv://xxxx:xxxxxx@cluster0.gyxjb.mongodb.net/xxxxx?retryWrites=true&w=majority',
     token_key: 'jwt_ubereats',
-    // awsAccessKey: "AKIAQYDJN6YJATVACL6O",
-    // awsSecretKey: "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",
-    bucketName: "ubereatsayush",
+    bucketName: "xxxxx",
     bucketRegion: "us-east-2",
-    awsAccessKey: "AKIA5WQUGNR72TFV7UUD",
-    awsSecretKey: "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",
+    awsAccessKey: "xxxxxxxx",
+    awsSecretKey: "xxxxxxxxx",
   }
 
 export default config;
